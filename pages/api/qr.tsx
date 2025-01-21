b7ea49949ca2b32c6cdbbf6949f4636686aee134
Stuart Kennedy <rav2040@gmail.com>
2025-01-21 16:50:50 +1300

    test

--- pages/api/qr.tsx
+++ pages/api/qr.tsx
@@ -1,13 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { toFileStream } from "qrcode";
+import { toBuffer } from "qrcode";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const textStr = String(req.query.text);
 
+    const result = await toBuffer(textStr);
+
     res.setHeader('Content-Type', 'image/png');
-
-    const result = await toFileStream(res, textStr);
-
+    res.setHeader('Content-Length', result.byteLength);
     res.status(200).end(result);
 }
